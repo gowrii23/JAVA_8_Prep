2a574c4ed9f3a62f4c78c76fc76f424b9d488773
gowrii23 <s.gowri.shankarrr@gmail.com>
2018-03-25 18:06:18 +0530

    Changes
    Few

--- JAVA_8_Prep/src/objects/test/Animal.java
+++ JAVA_8_Prep/src/objects/test/Animal.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 
 public class Animal {
 
+	//override::  must not be <<static>> or <<final>> for overridded by Sub-classes
+	//override:: must not be private, - access restriction
 	public void eat() throws IOException{
 		System.out.println("Animal Eat");
 	}
--- JAVA_8_Prep/src/objects/test/Horse.java
+++ JAVA_8_Prep/src/objects/test/Horse.java
@@ -3,7 +3,9 @@ package objects.test;
 import java.io.IOException;
 
 public class Horse extends Animal{
-
+	
+	//Overridden:: Cannot have Private access specifier in Override Method
+	//Overridden:: Exception must be more specific Eg: It can have IOException or more specifc IOExc
 	public void eat() throws IOException {
 		System.out.println("Horse Eats Oats");
 	}
