5aa4a4d24e4d8a60b5dd962e98b70a0eec1b6259
gowrii23 <s.gowri.shankarrr@gmail.com>
2018-03-25 17:32:25 +0530

    Added
    Few lines

--- JAVA_8_Prep/src/objects/test/TestAnimal.java
+++ JAVA_8_Prep/src/objects/test/TestAnimal.java
@@ -9,11 +9,22 @@ public class TestAnimal {
 		ani.eat();
 		hor.eat();
 		
-		//Polymorphism 
+		//Polymorphism 1
 		Animal pAni = new Horse();  // Horse IS-A Animal so it will work
 		pAni.eat();
 		//Horse pHor = new Animal(); //Error //Type mismatch: cannot convert from Animal to Horse
+		
+		//Polymorphism 2
+		sleep(hor);
+		
 		System.out.println("HI");
 		
+	}
+
+	
+	//Polymorphism 2
+	public static void sleep(Animal ani) {
+		// TODO Auto-generated method stub
+		System.out.println("Polymorphism:: Though Horse obj is passed it picks");
 	}
 }
